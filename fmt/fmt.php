c19ed91a0e3b8c4d2c99116b9516ab736d221714
gas <gasweld@gmail.com>
2016-10-25 00:03:35 +0300

    Fix ambiguous condition

--- fmt/fmt.php
+++ fmt/fmt.php
@@ -102,16 +102,33 @@ class fmt
 		 * Separate by space from both sides
 		 */
 		$lrspace = array(
-			'=', '==', '===', '!=', '!==',
-			'<', '>', '<=', '>=',
-			'+', '-', '*', '/', '%',
-			'+=', '-=', '*=', '%=', '.=',
-			'=>',
-			'||', '&&',
-			'?', ':',
-			"as", "instanceof"
+			T_AND_EQUAL,
+			T_AS,
+			T_BOOLEAN_AND,
+			T_BOOLEAN_OR,
+			T_CONCAT_EQUAL,
+			T_DIV_EQUAL,
+			T_DOUBLE_ARROW,
+			T_INSTANCEOF,
+			T_IS_EQUAL,
+			T_IS_GREATER_OR_EQUAL,
+			T_IS_IDENTICAL,
+			T_IS_NOT_EQUAL,
+			T_IS_NOT_IDENTICAL,
+			T_IS_SMALLER_OR_EQUAL,
+			T_MINUS_EQUAL,
+			T_MOD_EQUAL,
+			T_MUL_EQUAL,
+			T_OR_EQUAL,
+			T_PLUS_EQUAL,
+			T_SL,
+			T_SL_EQUAL,
+			T_SR,
+			T_SR_EQUAL,
+			T_XOR_EQUAL,
+			'=', '<', '>', '+', '-', '*', '/', '%', '?', ':'
 		);
-		if(in_array($tok[1], $lrspace)) {
+		if(in_array($tok[0], $lrspace)) {
 			out::str(' '.$tok[1].' ');
 			return;
 		}
